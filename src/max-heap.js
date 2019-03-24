3377dd0489818602c6eb2c83c395a1c2df2b0bf0
Yaroslav <44002588+BarikQ@users.noreply.github.com>
2019-03-24 21:52:59 +0300

    Update max-heap.js

--- src/max-heap.js
+++ src/max-heap.js
@@ -14,27 +14,9 @@ class MaxHeap {
 	}
 
 	pop() {
-		if(this.root!=null){
-		 var x=this.root.data;
-		 var detached=new Node(null,null);		 
-         detached=this.detachRoot();		 
-		 if(this.parentNodes.length>0) {	 
-			 this.restoreRootFromLastInsertedNode(detached);
-		     this.shiftNodeDown(this.root);
-		     }
-		 return x;						
-		}
 	}
 
 	detachRoot() {
-		var x=new Node(null,null);
-		x=this.root;
-		if (this.parentNodes.indexOf(this.root)>=0) this.parentNodes.splice(this.parentNodes.indexOf(this.root),1);
-		this.nodes.splice(this.nodes.indexOf(x),1);			
-		if (this.parentNodes.indexOf(this.root)>=0) this.parentNodes.splice(this.parentNodes.indexOf(this.root),1);
-		
-		this.root=null;
-		return x
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
